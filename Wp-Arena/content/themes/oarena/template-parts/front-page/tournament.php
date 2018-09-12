72f5fd7720aa81d64538543043fd5c1f4d8c2e55
Bert Parr <bertrand.parroche@gmail.com>
2018-09-12 16:50:32 +0200

    routage OK

---
+++ Wp-Arena/content/themes/oarena/template-parts/front-page/tournament.php
@@ -0,0 +1,16 @@
+    <div class="card mb-2 p-3" style="width: 15rem;">
+        <div class="card__intro d-flex justify-content-around ">
+        <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
+        <ul class="mt-2 ml-2">
+            <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
+            <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
+            <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
+            <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
+        </ul>
+        </div>
+        <div class="card-body d-flex flex-column">
+        <h3 class="news__tournament">Tournoi de Noël</h3>
+        <p class="card-text"> Venez affronter vos amis dans ce tournoi de Counter-strike.</p>
+           <a href="<?= get_the_permalink(get_the_ID()) ; ?>" class="text-center"><button type="button" class="btn btn-info" >Infos</button></a>
+        </div>
+    </div>
--- Wp-Arena/content/themes/oarena/template-parts/front-page/tournaments.php
+++ Wp-Arena/content/themes/oarena/template-parts/front-page/tournaments.php
@@ -1,52 +1,27 @@
 <h2 class="news__title text-light mt-3 mb-3 text-center">&#9660; PROCHAINS TOURNOIS &#9660; </h2>
+    <div class="cards d-flex justify-content-around flex-wrap">
 
-<div class="cards d-flex justify-content-around flex-wrap">
-    <div class="card mb-2 p-3" style="width: 15rem;">
-        <div class="card__intro d-flex justify-content-around ">
-        <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
-        <ul class="mt-2 ml-2">
-            <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
-            <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
-            <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
-            <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
-        </ul>
-        </div>
-        <div class="card-body d-flex flex-column">
-        <h3 class="news__tournament">Tournoi de Noël</h3>
-        <p class="card-text"> Venez affronter vos amis dans ce tournoi de Counter-strike.</p>
-            <button type="button" class="btn btn-info">Infos</button>
-        </div>
-    </div>
-    <div class="card mb-2 p-3" style="width: 15rem;">
-        <div class="card__intro d-flex justify-content-around ">
-        <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
-        <ul class="mt-2 ml-2">
-            <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
-            <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
-            <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
-            <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
-        </ul>
-        </div>
-        <div class="card-body d-flex flex-column">
-        <h3 class="news__tournament">Tournoi de la Toussaint</h3>
-        <p class="card-text">Le tournoi des loosers sur Fortnite.</p>
-            <button type="button" class="btn btn-info">Infos</button>
-        </div>
-    </div>   
-     <div class="card mb-2 p-3" style="width: 15rem;">
-        <div class="card__intro d-flex justify-content-around ">
-        <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
-        <ul class="mt-2 ml-2">
-            <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
-            <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
-            <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
-            <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
-        </ul>
-        </div>
-        <div class="card-body d-flex flex-column">
-        <h3 class="news__tournament">Tournoi du 1er janvier</h3>
-        <p class="card-text">Venez nolifer au tournoi FIFA 18 en attendant le nouvel opus</p>
-            <button type="button" class="btn btn-info">Infos</button>
-        </div>
-    </div>
+            <?php 
+
+            $args= [
+                'post_type' => 'tournament',
+                'cat' => '',
+                'posts_per_page' => 4,
+                'order' => 'DESC',
+                'order_by' => 'date'
+            ];
+
+            $our_articles = new WP_Query($args);
+
+            if ($our_articles->have_posts()): while ($our_articles->have_posts()): $our_articles->the_post(); 
+
+            get_template_part('template-parts/front-page/tournament');
+
+            endwhile;
+
+            // Je remet les variables globals telle qu'elles étaient avant ma boucle custom
+            wp_reset_postdata();    
+
+            endif; ?>
+
 </div>
