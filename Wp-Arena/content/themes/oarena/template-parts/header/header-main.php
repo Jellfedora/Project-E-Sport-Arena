4e7709f036335da03983e12323ae93c19563340f
Bert Parr <bertrand.parroche@gmail.com>
2018-09-12 12:22:11 +0200

    refonte nav

--- Wp-Arena/content/themes/oarena/template-parts/header/header-main.php
+++ Wp-Arena/content/themes/oarena/template-parts/header/header-main.php
@@ -1,37 +1,37 @@
 <header>
-  <nav class="nav navbar navbar-expand-lg navbar-light">
-    <!-- <a class="navbar-brand" href="#">oarena</a> -->
-    <div class="nav__logo d-none-md d-none d-lg-block"><img src="http://esportarena.fr/wp-content/uploads/2016/10/defaut-1-300x132.png"></div>
-    <h1 class="nav__title">oarena</h1>
-    <img class="nav__image" src="" alt="">
-    <button class="nav navbar-toggler nav__burger" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-      aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="nav__content collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="nav__links navbar-nav">
-        <li class="nav__item ">
-          <a class="nav__link nav-link" href="#">Home</a>
-        </li>
-        <li class="nav__item nav-item">
-          <a class="nav__link nav-link" href="#">Billeterie</a>
-        </li>
-        <li class="nav__item nav-item">
-          <a class="nav__link nav-link" href="#">Jeux</a>
-        </li>
-        <li class="nav__item nav-item">
-          <a class="nav__link nav-link" href="#">Teams</a>
-        </li>
-        <li class="nav__item nav-item">
-          <a class="nav__link nav-link" href="#">&Eacute;venement</a>
-        </li>
-        <li class="nav__item nav-item">
-          <a class="nav__link nav-link" href="#">Galerie</a>
-        </li>
-        <li class="nav__item nav-item">
-          <a class="nav__link nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+     <div class="nav navbar navbar-expand-lg navbar">
+      <!-- <a class="navbar-brand" href="#">oarena</a> -->
+      <div class="nav__logo d-none-md d-none d-lg-block"><img src="http://esportarena.fr/wp-content/uploads/2016/10/defaut-1-300x132.png"></div>
+      <h1 class="nav__title">oarena</h1>
+      <img class="nav__image" src="" alt="">
+      <button class="nav navbar-toggler nav__burger" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <nav class="nav__content collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="nav__links navbar-nav">
+            <li class="nav__item ">
+              <a class="nav__link nav-link" href="#">Home</a>
+            </li>
+            <li class="nav__item nav-item">
+              <a class="nav__link nav-link" href="#">Billeterie</a>
+            </li>
+            <li class="nav__item nav-item">
+              <a class="nav__link nav-link" href="#">Jeux</a>
+            </li>
+            <li class="nav__item nav-item">
+              <a class="nav__link nav-link" href="#">Teams</a>
+            </li>
+            <li class="nav__item nav-item">
+              <a class="nav__link nav-link" href="#">&Eacute;venement</a>
+            </li>
+            <li class="nav__item nav-item">
+              <a class="nav__link nav-link" href="#">Galerie</a>
+            </li>
+            <li class="nav__item nav-item">
+              <a class="nav__link nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+  </div>
 </header>
