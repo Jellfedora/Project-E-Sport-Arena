1e63f00302a866f7446836fc98592ee51eb4f735
Bert Parr <bertrand.parroche@gmail.com>
2018-09-12 15:37:45 +0200

    nouvelles cartes tournament

--- Wp-Arena/content/themes/oarena/template-parts/front-page/tournaments.php
+++ Wp-Arena/content/themes/oarena/template-parts/front-page/tournaments.php
@@ -1,4 +1,4 @@
-<h2 class="news__title text-light mb-3 text-center">&#9660; PROCHAINS TOURNOIS &#9660; </h2>
+<h2 class="news__title text-light mt-3 mb-3 text-center">&#9660; PROCHAINS TOURNOIS &#9660; </h2>
 
 <div class="cards d-flex justify-content-around flex-wrap">
     <div class="card mb-2 p-3" style="width: 15rem;">
@@ -11,15 +11,15 @@
             <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
         </ul>
         </div>
-        <div class="card-body">
+        <div class="card-body d-flex flex-column">
         <h3 class="news__tournament">Tournoi de Noël</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.</p>
+        <p class="card-text"> Venez affronter vos amis dans ce tournoi de Counter-strike.</p>
+            <button type="button" class="btn btn-info">Infos</button>
         </div>
     </div>
     <div class="card mb-2 p-3" style="width: 15rem;">
         <div class="card__intro d-flex justify-content-around ">
-        <img class="card-img-top" src="https://source.unsplash.com/100x100/?videogame">
+        <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
         <ul class="mt-2 ml-2">
             <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
             <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
@@ -27,28 +27,26 @@
             <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
         </ul>
         </div>
-        <div class="card-body">
-        <h3 class="news__tournament">Tournoi du 1er janvier</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-        </p>
-        </div>
-    </div>
-    <div class="card mb-2 p-3" style="width: 15rem;">
-        <div class="card__intro d-flex justify-content-around ">
-        <img class="card-img-top" src="https://source.unsplash.com/100x100/?gamer">
-        <ul class="mt-2 ml-2">
-            <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
-            <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
-            <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
-            <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
-        </ul>
-        </div>
-        <div class="card-body">
+        <div class="card-body d-flex flex-column">
         <h3 class="news__tournament">Tournoi de la Toussaint</h3>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-        </p>
+        <p class="card-text">Le tournoi des loosers sur Fortnite.</p>
+            <button type="button" class="btn btn-info">Infos</button>
+        </div>
+    </div>   
+     <div class="card mb-2 p-3" style="width: 15rem;">
+        <div class="card__intro d-flex justify-content-around ">
+        <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
+        <ul class="mt-2 ml-2">
+            <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
+            <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
+            <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
+            <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
+        </ul>
+        </div>
+        <div class="card-body d-flex flex-column">
+        <h3 class="news__tournament">Tournoi du 1er janvier</h3>
+        <p class="card-text">Venez nolifer au tournoi FIFA 18 en attendant le nouvel opus</p>
+            <button type="button" class="btn btn-info">Infos</button>
         </div>
     </div>
 </div>
