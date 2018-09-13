00fc71729032da17406dd63a3eefcd9ddd6772cc
Bert Parr <bertrand.parroche@gmail.com>
2018-09-13 14:01:03 +0200

    fix_article_final

--- Wp-Arena/content/themes/oarena/template-parts/posts/posts-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/posts/posts-display.php
@@ -1,13 +1,14 @@
-    <div class="news__item">
-            <div class="news__item-image" style="background-image: url(<?= get_the_post_thumbnail_url(); ?>);">
-            </div>
-            <span class="news__items-type">League of legends</span>
-            <div class="news__item-content  ">
-                <a href="#">
-                <h3 class="news__item-title text-light"><?php the_title(); ?></h3><div class="category-button d-flex justify-content-center ml-5">
-                <?php the_category() ; ?>
-            </div>
-            </a>
-            <p class="news__item-text"><?php the_content() ; ?></p>
-    </div>
+<div class="news__item">
+        <div class="news__item-image" style="background-image: url(<?= get_the_post_thumbnail_url(); ?>);">
+        </div>
+        <span class="news__items-type">League of legends</span>
+        <div class="news__item-content  ">
+            <a href="#">
+            <h3 class="news__item-title text-light"><?php the_title(); ?></h3>
+        <div class="category-button d-flex justify-content-center ml-5">
+            <?php the_category() ; ?>
+        </div>
+        </a>
+        <p class="news__item-text"><?php the_content() ; ?></p>
+        </div>
 </div>
