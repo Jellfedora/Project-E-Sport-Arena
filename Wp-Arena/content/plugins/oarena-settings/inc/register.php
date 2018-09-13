db44f29f26cf83ca97f45d8c62481ab9ce3dd35c
Bert Parr <bertrand.parroche@gmail.com>
2018-09-13 08:40:11 +0200

    nav

--- Wp-Arena/content/plugins/oarena-settings/inc/register.php
+++ Wp-Arena/content/plugins/oarena-settings/inc/register.php
@@ -1,9 +1,7 @@
 <?php
+
 Class oArena_register
 {
-    public $user_name='';
-    public $user_email='';
-
     public function __construct(){
         
         $this->addUser();
@@ -11,9 +9,10 @@ Class oArena_register
 
     public function addUser()
     {
-        
-    //$user_name='test';
-    //$user_email='test@mail.com';
+    $user_name='test7';
+    $user_email='test7@mail.com';
+    //$user_email='';
+    //$user_name='';
     $user_id = username_exists( $user_name );
 
     if ( !$user_id and email_exists($user_email) == false ) {
@@ -39,4 +38,36 @@ Class oArena_register
         }
     }
 }
+
+
+
+
+//$user_email='test@test.fr';
+//$user_name ='test';
+//$user_id = username_exists( $user_name );
+//if ( !$user_id and email_exists($user_email) == false ) {
+//	$random_password = wp_generate_password( 12, false );
+//	$user_id = wp_create_user( $user_name, $random_password, $user_email );
+//} else {
+//	$random_password = __('User already exists.  Password inherited.');
+//}
+
+
+//$userdata = array(
+//    'user_pass' => $random_password,
+//    'user_login' => $user_name,
+//    'user_email' => $user_email,
+//    'user_url'  => site_url( '/' ),
+//    'first_name' => 'Amine',
+//    'last_name'  => 'Betari',
+//);
+
+//$user_id = wp_insert_user( $userdata ) ;
+
+////On success
+//if( !is_wp_error($user_id) ) {
+// echo "User created : ". $user_id;
+//} 
+
+
 ?>
