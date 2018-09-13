3822ceff786529fd797a618d8f7e5b990c3b21c1
Julien <julienlecointe@live.fr>
2018-09-13 23:50:04 +0200

    ajout de conditions

--- Wp-Arena/content/themes/oarena/page-register.php
+++ Wp-Arena/content/themes/oarena/page-register.php
@@ -9,20 +9,20 @@
     
     <div class="form-group">
         <label for="email">Email *</label>
-        <input value="<?php $user_email ?>" type="email" class="form-control" id="email" name="email"  aria-describedby="emailHelp" placeholder="Adresse email" required>
+        <input type="email" class="form-control" id="email" name="email"  aria-describedby="emailHelp" placeholder="Adresse email" required>
         <small id="emailHelp" class="form-text text-muted">votre adresse email restera secrète. Elle ne sera rendue publique qu'aux inscrits à un évènement dont vous seriez l'organisateur</small>
     </div>
     
     <div class="form-group">
         <label for="username">Nom d'utilisateur *</label>
-        <input value="<?php $user_name ?>" type="text" class="form-control" id="username" name="username"  aria-describedby="usernameHelp" placeholder="Nom d'utilisateur" required>
+        <input type="text" class="form-control" id="username" name="username"  aria-describedby="usernameHelp" placeholder="Nom d'utilisateur" required>
         <small id="usernameHelp" class="form-text text-muted">en minuscules et sans espaces ni caractères spéciaux</small>
     </div>
 
     <div class="form-group">
         <small class="form-text text-muted">* champs obligatoires</small>
     </div>
-    <button class="btn btn-block btn-primary">Valider</button>
+     <input type="submit" name="envoi">
 </form>
 
 <?php //$register = new oArena_register(); ?>
