ae68ad6f97d2b20d0508132d34dc7da4f78eaa9b
Bert Parr <bertrand.parroche@gmail.com>
2018-09-14 15:50:27 +0200

    fix archiveteam archive tournoi

---
+++ Wp-Arena/content/themes/oarena/single-team.php
@@ -0,0 +1,14 @@
+   <?php get_header(); ?>
+   
+       <?php
+
+            if (have_posts()): while (have_posts()): the_post();
+
+                get_template_part('template-parts/team/team', 'display');
+
+            endwhile; wp_reset_postdata(); endif;
+
+        ?>
+    </main>
+
+    <?php get_footer() ; ?>
---
+++ Wp-Arena/content/themes/oarena/template-parts/team/team-display.php
@@ -0,0 +1,16 @@
+<div class="card mb-2 p-3" style="width: 15rem;">
+    <div class="card__intro d-flex justify-content-around ">
+    <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
+    <ul class="mt-2 ml-2">
+        <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> Jeux</span></i>
+        <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> Places</span></i>
+        <i class="fa fa-eur " aria-hidden="true"><span class="intro__font ml-1"> Prix</span></i>
+        <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> Date</span></i>
+    </ul>
+    </div>
+    <div class="card-body d-flex flex-column">
+    <h3 class="news__tournament"><?php the_title() ; ?></h3>
+    <p class="card-text"> <?php the_content() ; ?></p>
+        <a href="<?= get_the_permalink(get_the_ID()) ; ?>" class="text-center"><button type="button" class="btn btn-info" >S'inscrire</button></a>
+    </div>
+</div>
