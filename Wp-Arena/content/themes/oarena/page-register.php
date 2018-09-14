d438d53d284497a82cd24e140252d571ad0f4cae
Julien <julienlecointe@live.fr>
2018-09-14 15:11:53 +0200

    modif formulaire inscription

--- Wp-Arena/content/themes/oarena/page-register.php
+++ Wp-Arena/content/themes/oarena/page-register.php
@@ -6,13 +6,6 @@
 <form action=" register.php " class="col-12 col-md-6 offset-md-3 mb-5" method="post">
     <h1>Inscription</h1>
     
-    
-    <div class="form-group">
-        <label for="email">Email *</label>
-        <input type="email" class="form-control" id="email" name="email"  aria-describedby="emailHelp" placeholder="Adresse email" required>
-        <small id="emailHelp" class="form-text text-muted">votre adresse email restera secrète</small>
-    </div>
-    
     <div class="form-group">
         <label for="username">Nom d'utilisateur *</label>
         <input type="text" class="form-control" id="username" name="username"  aria-describedby="usernameHelp" placeholder="Nom d'utilisateur" required>
@@ -20,10 +13,17 @@
     </div>
 
     <div class="form-group">
+        <label for="email">Email *</label>
+        <input type="email" class="form-control" id="email" name="email"  aria-describedby="emailHelp" placeholder="Adresse email" required>
+        <small id="emailHelp" class="form-text text-muted">votre adresse email restera secrète</small>
+    </div>
+    
+    <div class="form-group">
         <label for="password">Mot de passe *</label>
         <input type="password" class="form-control" id="password" name="password" aria-describedby="passwordHelp" placeholder="Choisissez votre mot de passe" required>
         <small id="passwordHelp" class="form-text text-muted">8 caractères minimum</small>
     </div>
+    
     <div class="form-group">
         <label for="password2">Confirmation *</label>
         <input type="password" class="form-control" id="password2" name="password2" placeholder="Confirmation du mot de passe" required>
