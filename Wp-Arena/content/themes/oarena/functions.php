ccc15fc2a211f34dea07f9b82d1bf1727f8911e0
Julien <julienlecointe@live.fr>
2018-09-17 10:07:15 +0200

    ajout templates tags pour tournament

--- Wp-Arena/content/themes/oarena/functions.php
+++ Wp-Arena/content/themes/oarena/functions.php
@@ -1,9 +1,11 @@
-<?php 
+<?php
 
 // Dossier pour les require des fonctions ( découpage en plusieurs fichiers )
 
-require('inc/theme-setup.php');
+require 'inc/theme-setup.php';
 
-require('inc/theme-cleaner.php');
+require 'inc/theme-cleaner.php';
 
-require('inc/theme-enqueue.php');
+require 'inc/theme-enqueue.php';
+
+require 'inc/theme-template-tags.php';
