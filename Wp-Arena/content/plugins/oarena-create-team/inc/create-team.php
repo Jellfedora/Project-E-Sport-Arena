83299ca1533e29a617837b91aeea15f3f127fe5c
Bert Parr <bertrand.parroche@gmail.com>
2018-09-18 11:05:07 +0200

    ajout plugin au gitignore

---
+++ Wp-Arena/content/plugins/oarena-create-team/inc/create-team.php
@@ -0,0 +1,43 @@
+<?php
+
+Class oArenaCreateTeam
+{
+    public function __construct(){
+        
+        add_action ('init', [$this, 'addTeam']);
+    }
+
+    public function addTeam()
+    {
+
+    $errorList2 = [];    
+
+    if (isset($_POST['submit-team'])) 
+    {
+    // htmlspecialchars convertit les caractères spéciaux en entités HTML
+    $teamName = htmlspecialchars($_POST['team-name']);
+    // fonction wp qui 'néttoie' le contenu du post
+    $myteamdescription = wp_kses_post($_POST['myteamdescription']);
+    // Create content in BDD
+    if (empty($teamName)) {
+        $errorList2[] = 'Ne peux pas etre vide';        
+        var_dump($errorList2);
+        exit;
+    }
+    $team_post = [
+        'post_title' => $teamName,
+        'post_content' => $myteamdescription,
+        'post_type' => 'team',
+        'post_status' => 'publish',
+    ];
+
+    wp_insert_post($team_post);
+    // Je récupére l'ID du membre
+    $user_id = get_current_user_id();
+    // Je lui change son role en team admin
+    wp_update_user([
+        'ID' => $user_id,
+        'role' => 'TeamAdmin',
+    ]);
+    }}
+}
---
+++ Wp-Arena/content/plugins/oarena-create-team/oarena-create-team.php
@@ -0,0 +1,19 @@
+ <?php  
+/*
+Plugin Name: oArena Create Team
+Description: Création d'équipe 
+Author: Julien Lecointe Bertrand PARROCHE
+Version: 1.0.0
+*/
+
+    if (!defined('WPINC')) {die();}
+    
+    require plugin_dir_path(__FILE__). 'inc/create-team.php';
+    
+    // Instantiation of oArenaCreateTeam
+    $oArenaCreateTeam = new oArenaCreateTeam();
+    
+    
+    register_activation_hook(__FILE__, [$oArenaCreateTeam, 'activation']);
+    register_deactivation_hook(__FILE__, [$oArenaCreateTeam, 'deactivation']);
+ ?>
