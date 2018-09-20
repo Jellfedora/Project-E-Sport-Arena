202257ea520f1dcda92ac01ea25ab7b5a1553ffb
Julien <julienlecointe@live.fr>
2018-09-20 15:30:49 +0200

    style bouton menu utilisateur

--- Wp-Arena/content/themes/oarena/template-parts/header/header-nav.php
+++ Wp-Arena/content/themes/oarena/template-parts/header/header-nav.php
@@ -39,7 +39,17 @@ if (is_user_logged_in())
             };
         } else {
        
-    echo $menu;
+     $menu = wp_nav_menu([
+                'theme_location' => 'header',
+                'container' => 'nav',
+                'container_class' => 'nav__content collapse navbar-collapse',
+                'echo' => false,
+                ]);
+                
+                $menu = strip_tags($menu, '<a><li>');
+                $menu = str_replace('a href', 'a class="nav__link nav-link" href', $menu);
+                
+                echo $menu;
 }
 
 
