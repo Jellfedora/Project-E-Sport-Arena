400e58a9a3bd7c01e0a48d0c346e0aee18bcaca4
Bert Parr <bertrand.parroche@gmail.com>
2018-09-21 10:15:55 +0200

    recuperation articles ok

--- Wp-Arena/content/plugins/oarena-create-team/inc/create-team.php
+++ Wp-Arena/content/plugins/oarena-create-team/inc/create-team.php
@@ -14,72 +14,88 @@ Class oArenaCreateTeam
         $args= [
             'post_type' => 'team',
             'posts_per_page' => -1,
-            'post_status' => 'publish'
+            'post_status' => 'publish',
+            'title'=> get_the_title()
         ];
         
         $our_articles = new WP_Query($args);
         
         if ($our_articles->have_posts()): while ($our_articles->have_posts()): $our_articles->the_post(); 
+        var_dump(get_post());
         
         
-        $post[] = get_the_title();
+        endwhile;
+                // Je remet les variables globals telle qu'elles étaient avant ma boucle custom
+                // wp_reset_postdata();    
+                
+                endif; 
+        
+        
+        
+        // $post = get_the_title();
+        // die;
+        
+        
+        
+        
+        // $post = get_custom_posts();
         // var_dump($post);
-        foreach($post as $title){
+        // die;
+        
+        
+        // $post[] = get_the_title();
+        // var_dump($post);
+        // foreach($post as $title){
             // var_dump( $title);
+            // }
+            // echo the_title();
             
-        }
-        
-    endwhile;
-    // Je remet les variables globals telle qu'elles étaient avant ma boucle custom
-    wp_reset_postdata();    
-    
-    endif; 
-        
-    
-
-    $errorList = [];    
-
-        if (isset($_POST['submit-team'])) 
-        {
-        // htmlspecialchars convertit les caractères spéciaux en entités HTML
-        $teamName = htmlspecialchars($_POST['team-name']);
-        // fonction wp qui 'néttoie' le contenu du post
-        $myteamdescription = wp_kses_post($_POST['myteamdescription']);
-        // Create content in BDD
-        if (empty($teamName)) {
-            $errorList[] = 'Ne peux pas etre vide';
-            var_dump( $errorList);
-            // die;
-        }
-
-        if (empty($myteamdescription)) {
-            $errorList[] = 'Ne peux pas etre vide non plus';
-        } 
-        if ($title = $teamName ) {
-            $errorList[] = ' doublon';
-        }
-
-
-        $team_post = [
-            'post_title' => $teamName,
-            'post_content' => $myteamdescription,
-            'post_type' => 'team',
-            'post_status' => 'publish',
-        ];
-
-        // stocké en bdd dans wp-posts
-        wp_insert_post($team_post);
-        // Je récupére l'ID du membre
-        $user_id = get_current_user_id();
-        // Je lui change son role en team admin
-        wp_update_user([
-            'ID' => $user_id,
-            'role' => 'TeamAdmin',
-        ]);
-        // wp_redirect('gerer-son-equipe');exit;
-        }
-
-       
-    }
-    
+            
+            
+            $errorList = [];    
+            
+            if (isset($_POST['submit-team'])) 
+            {
+                // htmlspecialchars convertit les caractères spéciaux en entités HTML
+                $teamName = htmlspecialchars($_POST['team-name']);
+                // fonction wp qui 'néttoie' le contenu du post
+                $myteamdescription = wp_kses_post($_POST['myteamdescription']);
+                // Create content in BDD
+                if (empty($teamName)) {
+                    $errorList[] = 'Ne peux pas etre vide';
+                    var_dump( $errorList);
+                    // die;
+                }
+                
+                if (empty($myteamdescription)) {
+                    $errorList[] = 'Ne peux pas etre vide non plus';
+                } 
+                // if ($title === $teamName ) {
+                    //     $errorList[] = ' doublon';
+                    // }
+                    
+                    $team_post = [
+                        'post_title' => $teamName,
+                        'post_content' => $myteamdescription,
+                        'post_type' => 'team',
+                        'post_status' => 'publish',
+                    ];
+                    
+                    // stocké en bdd dans wp-posts
+                    wp_insert_post($team_post);
+                    // Je récupére l'ID du membre
+                    $user_id = get_current_user_id();
+                    // Je lui change son role en team admin
+                    wp_update_user([
+                        'ID' => $user_id,
+                        'role' => 'TeamAdmin',
+                        ]);
+                        // wp_redirect('gerer-son-equipe');exit;
+                    }
+                    
+                
+    }    
+                
+                
+                
 }
