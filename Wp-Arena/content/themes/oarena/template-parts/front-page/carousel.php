a9a987f80742b62b81c549d4d174dc790bd1442a
Julien <julienlecointe@live.fr>
2018-09-22 14:01:47 +0200

    inté carousel en cours

--- Wp-Arena/content/themes/oarena/template-parts/front-page/carousel.php
+++ Wp-Arena/content/themes/oarena/template-parts/front-page/carousel.php
@@ -2,28 +2,33 @@
     <div class="" data-ride="carousel" data-interval="3000">
       <div class="carousel-inner  ">
         <div class="carousel carousel-item active intro" style="background-image: url(http://geeko.lesoir.be/wp-content/uploads/sites/58/2018/08/esport.jpg);">
-          <div class="intro__small  " style="text-align: center;">
-            <div class="intro__small__title ">Tournoi de noel</div>
-            <div class="intro__small__button">
-              <button type="button" class="btn font-weight-bold btn-danger ">Infos</button>
+          <div class="intro__small text-center" >
+            <div class="intro__small__content">
+              <div class="intro__small__title ">Tournoi de noel</div>
+              <div class="intro__small__button">
+              <button type="button" class=" font-weight-bold  ">Infos</button>
+              </div>
             </div>
           </div>
         </div>
-        <div class="carousel carousel-item intro " style="background-image: url(https://fortunedotcom.files.wordpress.com/2015/01/riot-games-league-legends-finals-01.jpg);">
-          <div class=" intro__small" style="text-align: center;">
-            <div class="intro__small__title">Tournoi de la Toussaint</div>
-            <div class="intro__small__button">
-              <button type="button" class="btn font-weight-bold btn-danger ">Infos</button>
+        <div class="carousel carousel-item intro" style="background-image: url(https://fortunedotcom.files.wordpress.com/2015/01/riot-games-league-legends-finals-01.jpg);">
+          <div class="intro__small text-center" >
+            <div class="intro__small__content">
+              <div class="intro__small__title">Tournoi de la Toussaint</div>
+              <div class="intro__small__button">
+              <button type="button" class=" font-weight-bold  ">Infos</button>
             </div>
           </div>
+          </div>
         </div>
-        <div class="carousel carousel-item " style="background-image: url(https://www.challenges.fr/assets/img/2015/10/27/cover-r4x3w1000-578d78a2ec926-e-sport.jpg);">
-          <div class="
-            intro__small" style="text-align: center;">
-            <div class=" intro__small__title">Tournoi du 1er janvier</div>
-            <div class="intro__small__button">
-              <button type="button" class="btn font-weight-bold btn-danger ">Infos</button>
+        <div class="carousel carousel-item intro" style="background-image: url(https://comarketing-news.fr/wp-content/uploads/eSport-un-secteur-en-plein-developpement.jpg);">
+          <div class="intro__small text-center" >
+            <div class="intro__small__content">
+              <div class=" intro__small__title">Tournoi du 1er janvier</div>
+              <div class="intro__small__button">
+              <button type="button" class=" font-weight-bold  ">Infos</button>
             </div>
+          </div>
           </div>
         </div>
       </div>
