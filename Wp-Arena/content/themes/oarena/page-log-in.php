7f75b5a2ce540dea7a2609b03bc4107b046b2320
Julien <julienlecointe@live.fr>
2018-09-23 16:20:19 +0200

    intégration minimale formulaire de connexion via wp_login_forms

--- Wp-Arena/content/themes/oarena/page-log-in.php
+++ Wp-Arena/content/themes/oarena/page-log-in.php
@@ -1,42 +1,25 @@
-<?php get_header();
+<?php get_header();?>
 
-// Form of log in
-wp_login_form(array(
-    'redirect' => home_url(), // Return to home after login
-    'label_username' => 'Votre identifiant',
-    'label_password' => 'Votre mot de passe',
-    'label_remember' => 'Se souvenir de moi',
-    'label_log_in' => 'Se connecter',
-    'form_id' => 'login-form',
-    'id_username' => 'user-login',
-    'id_password' => 'user-pass',
-    'id_remember' => 'rememberme',
-    'id_submit' => 'wp-submit',
-    'remember' => true,
-    'value_remember' => false,
-));
+<?php get_template_part('template-parts/front-page/carousel') ; ?>
+<div class="loginform">
+<?php
+if ( ! is_user_logged_in() ) { // Display WordPress login form:
+    $args = array(
+        'redirect' => home_url(), 
+        'form_id' => 'loginform-custom',
+        'label_username' => __( 'Nom d\'utilisateur' ),
+        'label_password' => __( 'Mot de passe' ),
+        'label_remember' => __( 'Se souvenir de moi' ),
+        'label_log_in' => __( 'Valider' ),
+        'remember' => true
+    );
+    wp_login_form( $args );
+} else { // If logged in:
+    echo "Vous êtes déjà connecté ";
+}
 ?>
-<form action="login.php" class="col-12 col-md-6 offset-md-3" method="post" id="login-form">
-    <h1>Connexion (pas encore fonctionnel mais au dessus si ;)</h1>
-
-    <div id="alerts" class="alert alert-danger alert-dismissible" role="alert" style="display:none;">
-    </div>
-
-    <div class="form-group">
-        <label for="label_username">Nom d'utilisateur *</label>
-        <input type="text" class="form-control" id="id_username" name="username" value="" placeholder="Votre identifiant" required>
-    </div>
-    <div class="form-group">
-        <label for="password">Mot de passe *</label>
-        <input type="password" class="form-control" id="password" name="password" placeholder="Choisissez votre mot de passe" required>
-    </div>
-    <div class="form-group">
-        <small class="form-text text-muted">* champs obligatoires</small>
-    </div>
-    <button class="btn btn-block btn-primary">Valider</button>
-</form>
+</div>
 
 
-<a href="<?php echo wp_lostpassword_url(get_bloginfo('url')); ?>" title="Lost Password">J'ai perdu mon mot de passe</a>
 
 <?php get_footer();?>
