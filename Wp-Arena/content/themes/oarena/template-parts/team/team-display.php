239939ff035a51735ebb03b4b680bd755b2156bd
Bert Parr <bertrand.parroche@gmail.com>
2018-09-24 20:20:10 +0200

    ajout d un peu de style page equipe + display members + chief

--- Wp-Arena/content/themes/oarena/template-parts/team/team-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/team/team-display.php
@@ -1,6 +1,19 @@
-<div class="card mb-2 p-3" style="width: 15rem;">
+<?php 
+$post = get_post();
+foreach ($post as $post_id) {
+    $post_id = $post->ID;
+}
+// var_dump($post_id);
+// // die;
+?>
+<div style="padding-bottom: 3em;">
+<div class="card mb-2 p-3 m-auto w-75 rounded" style="background-color:#6994db ">
     <div class="card-body d-flex flex-column">
-    <h3 class="news__tournament"><?php the_title() ; ?></h3>
-    <p class="card-text"> <?php the_content() ; ?></p>
+        <h5 class="news__tournament">Team :&nbsp; <?php the_title() ; ?></h5>
+        <p class="card-text"> <?php the_content() ; ?></p>   
+        <div class="news__tournament__status__chief">Leader de l'équipe : </div>
+     <div class="news__tournament__status__chief__name"><?= get_post_meta($post_id,'_team_chief',true); ?></div>
+        <div class="news__tournament__status__members">Membres de l'équipe :</div>
+        <div class="news__tournament__status__members__name"><li><?= $member1 = get_post_meta($post_id,'_member1',true);?></li></div>
     </div>
 </div>
--- Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
@@ -6,25 +6,25 @@ $id = $post->ID;
 // die;
 ?>
 
-<div class="card mb-2 p-3 m-auto w-25">
-    <div class="card__intro d-flex justify-content-around ">
-    <img class="card-img-top p-1" src="https://source.unsplash.com/100x100/?fortnite">
-    <ul class="mt-2 ml-2">
+<div class="card mb-2 p-3 m-auto w-75 rounded" style="background-color:#6994db ">
+    <div class="card__intro d-flex  justify-content-start ">
+    <ul class="mt-2 ml-2 d-flex flex-column">
         <i class="fa fa-gamepad" aria-hidden="true"><span class="intro__font ml-1"> <?= get_game(get_the_ID()); ?></span></i>
-        <i class="fa fa-user " aria-hidden="true"><span class="intro__font ml-1"> <?= get_seats(get_the_ID()); ?></span></i>
+        <i class="fa fa-user mb-2 mt-2" aria-hidden="true"><span class="intro__font ml-1"> <?= get_seats(get_the_ID()); ?></span></i>
         <i class="fa fa-calendar " aria-hidden="true"><span class="intro__font ml-1"> <?= get_price(get_the_ID()); ?></span></i>
     </ul>
     </div>
     <div class="card-body d-flex flex-column">
-    <h4>team inscrites: </h4>
-    <li><?= get_post_meta($id, '_team_register', true) ; ?></li>
-    <h3 class="news__tournament"><?php the_title() ; ?></h3>
-    <p class="card-text"> <?php the_content() ; ?></p>   
+        <h3 class="news__tournament"><?php the_title() ; ?></h3>
+        <p class="card-text"> <?php the_content() ; ?></p>   
+        <h4>team inscrites: </h4>
+        <li><?= get_post_meta($id, '_team_register', true) ; ?></li>
     <form action="add-team" method="post">
-    <p class="text-align">Nom de votre team : </p>
+    <p class="text-align mt-5">Nom de votre team : </p>
     <input type="test" value="" name="register-team-tournament" >
     <button type="submit" class="btn mt-2"> Inscrire ma team</button>
     </form>
     </div>
 </div>
+
 
