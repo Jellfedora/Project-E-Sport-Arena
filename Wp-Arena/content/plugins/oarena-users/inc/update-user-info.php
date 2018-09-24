75a42f82cc46ee3c52e91c8e8db8643af34cbcf4
Julien <julienlecointe@live.fr>
2018-09-25 00:47:30 +0200

    update metaboxe lié au membre OKgit add ../../plugins/oarena-users/inc/update-user-info.php

---
+++ Wp-Arena/content/plugins/oarena-users/inc/update-user-info.php
@@ -0,0 +1,73 @@
+<?php
+
+class oArena_updateUserInfo
+{
+    public function __construct()
+    {
+
+        add_action('init', [$this, 'updateUserInfo']);
+    }
+    public function updateUserInfo()
+    {
+        // Récupéres l'utilisateur connecté
+        $current_user = wp_get_current_user();
+
+        // Récupéres le nom de l'utilisateur qui est aussi son nom de membre (cpt donc post_title)
+        $post_title = $current_user->user_nicename;
+
+        // On se connecte à la base de données du site pour récupérer l'id du post-title
+        global $wpdb; 
+
+        $team_id = $wpdb->get_results("
+        SELECT ID
+        FROM wp_posts
+        WHERE `post_title` = '$post_title'
+        ;
+        ");
+
+        // On récupéres l'id en string
+        foreach($team_id as $post){
+            $post->ID;
+        }
+
+        // Si le formulaire est soumit
+        if (isset($_POST['update-user-info-submit'])) {
+            // Si l'input est rempli
+            if (!empty($_POST['member-name'])) {
+                // On met à jour la métaboxe
+                update_post_meta($post->ID, 'member_name', sanitize_text_field($_POST['member-name']));
+            }
+            if (!empty($_POST['member-firstname'])) {
+                update_post_meta($post->ID, 'member_firstname', sanitize_text_field($_POST['member-firstname']));
+            }
+            if (isset($_POST['member-street'])) {
+                update_post_meta($post->ID, 'member_street', sanitize_text_field($_POST['member-street']));
+            }
+            if (isset($_POST['member-postal-code'])) {
+                update_post_meta($post->ID, 'member_postal_code', sanitize_text_field($_POST['member-postal-code']));
+            }
+            if (isset($_POST['member-city'])) {
+                update_post_meta($post->ID, 'member_city', sanitize_text_field($_POST['member-city']));
+            }
+            // Redirige vers la page profil
+                wp_redirect('profil');
+                exit;
+        }
+    }
+}
+
+
+
+
+// echo('hey1');exit;
+// add_action('save_post', 'save_member_info');
+// function save_member_info($post_id)
+// {
+//     echo('hey1');exit;
+//     // If the form is submitted
+//     if (isset($_POST['update-member-info-submit'])) {
+//         echo('hey');exit;
+//         // je stocke mes données dans des variables
+//         $user_name = ($_POST['user-name']);
+//         $user_email = ($_POST['user-email']);
+//     }
