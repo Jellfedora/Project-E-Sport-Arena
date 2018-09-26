091479f11f38d5306ff87075547720c05b7ee708
Julien <julienlecointe@live.fr>
2018-09-26 16:36:20 +0200

    buf update user name fixed!

--- Wp-Arena/content/plugins/oarena-users/inc/update-user.php
+++ Wp-Arena/content/plugins/oarena-users/inc/update-user.php
@@ -19,28 +19,14 @@ class oArena_updateUser
 
             // If the input user-name is !empty 
             if (!empty($user_name)) {
-
+                //echo('user name est renseigné');
                 //Ajout d'un tableau contenant les erreurs
                 $errorList = array();
-                $old_name = wp_get_current_user();
 
-                ####################################
-                //il faut stocker lancien nom pour changer celui du membre
-                $old_name->user_login;
-                
-                ####################################
 
-                //Récupére l'utilisateur courant et met à jour le nom du user
+                //Récupére l'utilisateur courant 
                 $current_user = wp_get_current_user();
 
-
-                ##############################
-
-                //var_dump($old_name->user_login);exit;
-                
-                //il faut mettre a jour le nom du membre avant son login
-                //Mettre à jour le nom du membre
-                
                 // Récupére les noms des membres sous forme d'un tableau:
                     
                     global $wpdb; // On se connecte à la base de données du site
@@ -51,23 +37,20 @@ class oArena_updateUser
                     WHERE `post_type` ='members' 
                     ;
                     ");
-                    
                     // Récupére les noms des membres en string et les stocke dans un tableau
                     $member_list_name= Array();
                     foreach ( $members_name  as $member_name ) {
                         $member_name->post_title;
                         array_push($member_list_name,$member_name->post_title);
                     }
-                    
-                    //var_dump($member_list_name);exit;
-                    
-                    
-                    //TODO récupérer le nom du membre connecté
-                    
+
+                    // Récupére le nom du membre connecté
                     $user_login = $current_user->user_login;
+
+                    //JUSQUE LA CA VA
+                    ###################################################
                     
                     
-                    //var_dump($user_login);exit;
                     
                     global $wpdb; // On se connecte à la base de données du site
                     
@@ -75,10 +58,10 @@ class oArena_updateUser
                     $member_id = $wpdb->get_results("
                     SELECT ID
                     FROM wp_posts
-                    WHERE `post_name` = '$user_login' AND `post_status`='publish'
+                    WHERE `post_title` = '$user_login' AND `post_status`='publish'
                     ;
                     ");
-                    
+                    //var_dump($member_id);exit;
                     
                     $id_member= '';
                     foreach($member_id as $id_member){
@@ -88,20 +71,21 @@ class oArena_updateUser
                     //var_dump($id_member);exit;
                     
 
-                    // Si le nom nest pas pris
-                    // Update le post-title ayant pour identifiant id_member
-                if (!in_array($user_name,($member_list_name))) {
+                    // Si le nom n'est pas pris
+                    if (!in_array($user_name,($member_list_name))) {
                     // Update title_post
                     $my_post = array(
                         'ID'           => $id_member,
                         'post_title'   => $user_name,
                     );
                     
-                    //echo('hey');exit;
-                    // Update the post into the database
-                    wp_update_post( $my_post );
-                    //var_dump($my_post);exit;
-                } else $errorList[] = 'Ce nom est déjà pris';
+                    
+                    // Update the post title into the database
+                    wp_update_post($my_post);
+                    
+                    echo('hey');
+                    
+                    
                 ###############################
                 $current_user->nickname = esc_attr($user_name);
                 // $current_user->user_login = esc_attr($user_name);
@@ -121,66 +105,45 @@ class oArena_updateUser
                 $user_id = get_current_user_id();
                 $wpdb->update($wpdb->users, array('user_login' => $user_name), array('ID' => $user_id));
                 
-                
-                
-                // global $current_user;
-                // get_currentuserinfo ();
-                // $current_username = $current_user->user_login;
-                // global $wpdb;
-                
-                // $wpdb->query( "
-                // UPDATE wp_users
-                // SET user_login = $user_name
-                // WHERE user_login = $current_username
-                // " );
-                // var_dump($current_username);exit;
-                ///////////////////////////////
-                
-                
+                //Récupérer toutes les adresses mail des utilisateurs et admin du site
 
-            } 
-
-
-            //Récupérer toutes les adresses mail des utilisateurs et admin du site
-
-            global $wpdb; // On se connecte à la base de données du site
+                global $wpdb; // On se connecte à la base de données du site
             
-            $list_email_users = $wpdb->get_results("
+                $list_email_users = $wpdb->get_results("
                 SELECT DISTINCT user_email
                 FROM wp_users
                 GROUP BY user_email
                 ;
                 ");
 
-            foreach($list_email_users as $mail){
-                $mail->user_email;
-                array_push($list_email_users,$mail->user_email);
+                foreach($list_email_users as $mail){
+                    $mail->user_email;
+                    array_push($list_email_users,$mail->user_email);
+                    }
+
+                //If the input user-email is !empty 
+                if ((!empty($user_email)) & (!in_array($user_email,($list_email_users)))) {
+                    $current_user = wp_get_current_user();
+                    //$mail = the_author_meta( 'email', $current_user->ID );
+                    // Update email_post
+                    $current_user->user_email = esc_attr($user_email);
+                    wp_update_user($current_user);
+                    //var_dump($current_user);exit;
+                } else if (!empty($user_email)) {
+                    $errorList[] = 'Cette adresse email est déjà prise!';
+                } 
+
+                // Si il y a des erreurs affiche les
+                if (!empty($errorList)) {
+                    var_dump($errorList);exit;
                 }
 
-            //var_dump($list_email_users);exit;
-
-
-            //If the input user-email is !empty 
-            if ((!empty($user_email)) & (!in_array($user_email,($list_email_users)))) {
-                $current_user = wp_get_current_user();
-                //$mail = the_author_meta( 'email', $current_user->ID );
-                // Update email_post
-                $current_user->user_email = esc_attr($user_email);
-                wp_update_user($current_user);
-                //var_dump($current_user);exit;
-            } else if (!empty($user_email)) {
-                $errorList[] = 'Cette adresse email est déjà prise!';
+                // Redirige vers la page profil
+                    wp_redirect('profil');
+                    exit;
+                } else $errorList[] = 'Ce nom est déjà pris';exit;
             } 
-
-            // Si il y a des erreurs affiche les
-            if (!empty($errorList)) {
-                var_dump($errorList);exit;
-            }
-
-            // Redirige vers la page profil
-                wp_redirect('profil');
-                exit;
-        } 
+        }
     }
 }
 
