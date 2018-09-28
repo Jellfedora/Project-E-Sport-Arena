09f66983943b104d332a92cf6f7e369c1afe46b4
Julien <julienlecointe@live.fr>
2018-09-28 10:38:22 +0200

    bug update participant as tournament atomized

--- Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
@@ -2,8 +2,7 @@
 
 $post = get_post();
 $id = $post->ID;
-// var_dump($id);
-// die;
+//var_dump($id);exit;
 ?>
 
 <div class="card mb-2 p-3 m-auto w-75 rounded" style="background-color:#6994db ">
@@ -18,7 +17,11 @@ $id = $post->ID;
         <h3 class="news__tournament"><?php the_title() ; ?></h3>
         <p class="card-text"> <?php the_content() ; ?></p>   
         <h4>team inscrites: </h4>
-        <li><?= get_post_meta($id, '_team_register', true) ; ?></li>
+        <?php  $register_teams = get_post_meta($post->ID, '_team_register');
+            foreach ($register_teams as $team) {
+            echo $team .'</br>'; 
+            }
+        ?>
     <form action="add-team" method="post">
     <p class="text-align mt-5">Nom de votre team : </p>
     <input type="test" value="" name="register-team-tournament" >
