5c2c5220957b554c7b7bc02dd8c642937952c81a
Julien <julienlecointe@live.fr>
2018-10-01 20:53:59 +0200

    integration + change quantity of cart

--- Wp-Arena/content/themes/oarena/template-parts/actualities/actualities-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/actualities/actualities-display.php
@@ -1,17 +1,16 @@
-<div class="pt-5">
-<div class="news__item d-flex text-left">
-        <div class="news__item-image" style="background-image: url(<?= get_the_post_thumbnail_url(); ?>);">
+<div class="actuality pt-5">
+    <div class="actuality__item  text-left" >
+        <div class="actuality__item__image" style="background-image: url(<?= get_the_post_thumbnail_url(); ?>);">
         </div>
-        <div class="news__item-content  ">
-            <a href="<?php the_permalink() ; ?>">
-            <h3 class="news__item-title text-light"><?php the_title(); ?></h3>
-            <div class="category-button d-flex justify-content-center ml-5">
-            <?php the_category() ; ?>
+        <div class="actuality__item__content">
+            <div class="actuality__item__category-button text-center">
+                <a href=""><?php the_category() ; ?></a>
             </div>
-            </a>
-            <div>
-            <?php the_content() ; ?>
+            <a href="<?php the_permalink() ; ?>"></a>
+            <h3 class="actuality__item__title text-light"><?php the_title(); ?></h3>
+            <div class="actuality__item__text">
+                <?php the_content() ; ?>
             </div>
         </div>
-</div>
+    </div>
 </div>
--- Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
@@ -4,70 +4,57 @@ $post = get_post();
 $id = $post->ID;
 //var_dump($id);exit;
 ?>
-<div class="pt-5">  
-    <div class="card mb-2 p-3 m-auto w-75 rounded " style="background-color:#6994db ">
-        <div class="card__intro d-flex  justify-content-start ">
-        <ul class="mt-2 ml-2 d-flex flex-column">
-          <div>  <?= get_price(get_the_ID()); ?><i class="fa fa-eur ml-2" aria-hidden="true"></i> </div>
-            <i> Places totales : <?= get_post_meta($id, 'tournament_available_total', true);?></i>
-            <i> Places restantes : <?= get_post_meta($id, 'tournament_available_seats', true);?></i>
-        </ul>
-        </div>
-        <div class="card-body d-flex flex-column">
-            <h3 class="news__tournament"><?php the_title() ; ?></h3>
-        
-                                    <?php  if(get_post_meta($id, 'tournament_available_seats', true) > 0 ){ ?>
-                                       
-         <h3 class="m-auto" style="color: green;">Il reste encore des places</h3>                              
-        
-                                    <?php } else { ?>
+<h2 class="d-none text-light  mb-3 text-center">&#9660; R&#201;PONDEZ &#192; L'APPEL ! &#9660; </h2>
 
+<div class="tournament " style="background-image:url(<?= the_post_thumbnail_url('full'); ?>);">  
+        <div class="tournament__content">
+            <h3 class="tournament__title"><?php the_title() ; ?></h3>
 
-        <h3 class="text-danger">Le tournoi est complet !</h3>
+<?php  if(get_post_meta($id, 'tournament_available_seats', true) > 0 ){ ?>
+            <p class="" style="color: green;">Il reste encore des places</p>
+<?php } else { ?>
+            <p class="text-danger">Le tournoi est complet !</p>
+<?php  } ?>
 
-                                  <?php  } ?>
+            <div class="tournament__meta-info">
+                <p>Places totales : <?= get_post_meta($id, 'tournament_available_total', true);?></p>
+                <p>Places restantes : <?= get_post_meta($id, 'tournament_available_seats', true);?></p>
+                <p>Prix:<?= get_price(get_the_ID()); ?><i class="fa fa-eur ml-2" aria-hidden="true"></i></p>
+            </div>
+            <div class="card-body d-flex flex-column">
+                <p class="card-text"> <?php the_content() ; ?></p>   
+                <h4>Team inscrites: </h4>
 
-            <p class="card-text"> <?php the_content() ; ?></p>   
-            <h4>Team inscrites: </h4>
-            <?php  $register_teams = get_post_meta($post->ID, '_team_register');
-                foreach ($register_teams as $team) {
-                echo $team .'</br>'; 
-                }
-            ?>
-        <form action="add-team" method="post">
-        <p class="text-align mt-5">Nom de votre team : </p>
-        <input type="text" value="" name="register-team-tournament" class="p-2" >
-        <input type="hidden" value="<?= get_the_ID(); ?>" name="register-tournament-id" >
+<?php   $register_teams = get_post_meta($post->ID, '_team_register');
+        foreach ($register_teams as $team) {
+            echo $team .'</br>'; 
+        }?>
 
-                <?php 
+            <form action="add-team" method="post">
+                <p class="text-align mt-5">Nom de votre team : </p>
+                <input type="text" value="" name="register-team-tournament" class="p-2" >
+                <input type="hidden" value="<?= get_the_ID(); ?>" name="register-tournament-id" >
 
-                        if (is_user_logged_in()) 
-                        {
-                            $user = wp_get_current_user()->roles;
+<?php 
 
-                                // var_dump($user);
-                                foreach ($user as $role) {
-                                    // var_dump($role);
-                                    // die;
-                                }
+        if (is_user_logged_in()) {
+            $user = wp_get_current_user()->roles;
+            foreach ($user as $role) {
+            }
+            if ($role === 'administrator' || $role === 'TeamAdmin') { ; 
+                if(get_post_meta($id, 'tournament_available_seats', true) > 0 )
+                { ?> 
+                <button type="submit" class="btn btn-success ml-2 mt-2 mr-2 mb-3"> Inscrire ma team</button>
+                <button name="remove-team" type="submit" class="btn btn-danger mt-2 mb-3" value="remove-team-submit"> Desinscrire ma team</button>
+            </form>
+<?php   } else { ?> 
+                <button name="remove-team" type="submit" class="btn btn-danger mt-2 mb-3" value="remove-team-submit"> Desinscrire ma team</button>
+                <p class="text-danger">Le tournoi est complet !</p>
+<?php           } 
 
-                                if ($role === 'administrator' || $role === 'TeamAdmin') 
-                                { ; 
-                                    if(get_post_meta($id, 'tournament_available_seats', true) > 0 )
-                                     { ?> 
-        <button type="submit" class="btn btn-success ml-2 mt-2 mr-2 mb-3"> Inscrire ma team</button>
-        <button name="remove-team" type="submit" class="btn btn-danger mt-2 mb-3" value="remove-team-submit"> Desinscrire ma team</button>
-        </form>
-                                    <?php } else { ?> 
-        <button name="remove-team" type="submit" class="btn btn-danger mt-2 mb-3" value="remove-team-submit"> Desinscrire ma team</button>
-        <p class="text-danger">Le tournoi est complet !</p>
-                                  <?php       } 
-
-                                    } 
-                                }  ?>
-                                
-        </div>
+         } 
+     }?>
+             
     </div>
 </div>
-
 
