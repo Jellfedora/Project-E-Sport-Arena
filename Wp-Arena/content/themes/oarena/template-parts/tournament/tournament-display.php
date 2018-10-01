4087c00ed9265f861b4c40070a10dae822b9c2a4
Julien <julienlecointe@live.fr>
2018-10-01 23:21:31 +0200

    maj

--- Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
+++ Wp-Arena/content/themes/oarena/template-parts/tournament/tournament-display.php
@@ -6,31 +6,34 @@ $id = $post->ID;
 ?>
 <h2 class="d-none text-light  mb-3 text-center">&#9660; R&#201;PONDEZ &#192; L'APPEL ! &#9660; </h2>
 
-<div class="tournament " style="background-image:url(<?= the_post_thumbnail_url('full'); ?>);">  
+<div class="tournament parallax" style="background-image:url(<?= the_post_thumbnail_url('full'); ?>);">  
         <div class="tournament__content">
-            <h3 class="tournament__title"><?php the_title() ; ?></h3>
+            <div class="tournament__header">
+                <h3 class="tournament__title"><?php the_title() ; ?></h3>
 
 <?php  if(get_post_meta($id, 'tournament_available_seats', true) > 0 ){ ?>
-            <p class="" style="color: green;">Il reste encore des places</p>
+                <p class="text-success">STATUT: INSCRIPTION OUVERTE</p>
 <?php } else { ?>
-            <p class="text-danger">Le tournoi est complet !</p>
+                <p class="text-danger">STATUT: INSCRIPTION FERME&#201;</p>
 <?php  } ?>
-
-            <div class="tournament__meta-info">
-                <p>Places totales : <?= get_post_meta($id, 'tournament_available_total', true);?></p>
-                <p>Places restantes : <?= get_post_meta($id, 'tournament_available_seats', true);?></p>
-                <p>Prix:<?= get_price(get_the_ID()); ?><i class="fa fa-eur ml-2" aria-hidden="true"></i></p>
             </div>
-            <div class="card-body d-flex flex-column">
-                <p class="card-text"> <?php the_content() ; ?></p>   
-                <h4>Team inscrites: </h4>
-
+            <div class="tournament__info">
+                <div class="tournament__info__content">
+                    <p class="card-text"> <?php the_content() ; ?></p> 
+                </div> 
+                <div class="tournament__info__meta">
+                    <p>Places totales : <?= get_post_meta($id, 'tournament_available_total', true);?></p>
+                    <p>Places restantes : <?= get_post_meta($id, 'tournament_available_seats', true);?></p>
+                    <p>Prix:<?= get_price(get_the_ID()); ?><i class="fa fa-eur ml-2" aria-hidden="true"></i></p>
+                </div>
+                     
 <?php   $register_teams = get_post_meta($post->ID, '_team_register');
         foreach ($register_teams as $team) {
             echo $team .'</br>'; 
         }?>
 
             <form action="add-team" method="post">
+                <h4>Team inscrites: </h4>
                 <p class="text-align mt-5">Nom de votre team : </p>
                 <input type="text" value="" name="register-team-tournament" class="p-2" >
                 <input type="hidden" value="<?= get_the_ID(); ?>" name="register-tournament-id" >
